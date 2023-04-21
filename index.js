5f2aa6aea25c5673156ce9b51e375d41466285ae
Yuhui <130331257+yuhuicaoo@users.noreply.github.com>
2023-04-21 23:09:25 +1200

    skeleton laoding
    added skeleton loading state between searches

--- index.js
+++ index.js
@@ -3,38 +3,36 @@
 const moviesWrapper = document.querySelector('.movies')
 moviesWrapper.classList += ' movies__loading'
 
-async function main(filter) {
+let movies
+
+async function renderMovies(id, filter) {
     const moviesWrapper = document.querySelector('.movies')
 
-    const movies = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=acf6e413&s=fast`)
+    if (!movies) {
+        movies = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=acf6e413&s=fast`)
+    }
+    else {
+        movies = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=acf6e413&s=${id}`)
+    }
+
     const moviesData = await movies.json()
     
     moviesWrapper.classList.remove('movies__loading')
     
-    const moviesList = moviesData.Search
+    const moviesList = moviesData.Search.slice(0,6)
 
     // Attempted to create a filter by release date , does not work for future movie searches.
     if (filter === 'NEW_TO_OLD') {
-        moviesList.sort((a,b) => a.Year - b.Year)
-    }
-    else if (filter === 'OLD_TO_NEW') {
         moviesList.sort((a,b) => b.Year - a.Year)
     }
-
-    moviesWrapper.innerHTML = moviesList.map((movie) => movieHTML(movie)).slice(0,6).join("")
+    else if (filter === 'OLD_TO_NEW') {
+        moviesList.sort((a,b) => a.Year - b.Year)
+    }
+    
+    moviesWrapper.innerHTML = moviesList.map((movie) => movieHTML(movie)).join("")
 }
 
-// Tried to make it not limited to movies with fast in it, wasnt too sure how to add skeleton loading states when searching for other movies.
-async function renderMovies(id) {
-    const movies = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=acf6e413&s=${id}`)
-    const moviesData = await movies.json()
-    
-    moviesWrapper.classList.remove('movies__loading')
-    
-    const moviesList = moviesData.Search
 
-    moviesWrapper.innerHTML = moviesList.map(movie => movieHTML(movie)).slice(0,6).join("")
-}
 
 function movieHTML(movie) {
     return `
@@ -55,7 +53,17 @@ function movieHTML(movie) {
 
 function onSearchChange(event) {
     searchResult(event.target.value)
-    renderMovies(event.target.value)
+    loadingBetweenSearch()
+    setTimeout(() => {
+        renderMovies(event.target.value)
+    }, 1000);
+}
+
+function loadingBetweenSearch() {
+    const divElement = document.querySelectorAll('.movie')
+    divElement.forEach(element => element.remove())
+    moviesWrapper.classList += ' movies__loading'
+    moviesWrapper.innerHTML = '<i class="fas fa-spinner movies__loading--spinner"></i>'
 }
 
 function searchResult(search) {
@@ -68,5 +76,6 @@ function filterMovies(event) {
 }
 
 setTimeout(() => {
-    main()
+    renderMovies()
 },1000)
+    
